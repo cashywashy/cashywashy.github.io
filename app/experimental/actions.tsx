db58e293f75ac3e9b588c677dd1bc7dfc5af8d11
Cashwash <55900491+cashywashy@users.noreply.github.com>
2025-11-02 13:44:56 -0500

    Delete app/experimental directory

--- app/experimental/actions.tsx
+++
@@ -1,6 +0,0 @@
-import { setData } from '../memory_storage';
-
-export async function data_store(formData:FormData) {
-    let bro_said = formData.get('bro_said');
-    setData('bro_said', bro_said);
-}
